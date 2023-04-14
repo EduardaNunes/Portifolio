6c7c5598ecadce1b2cda43e7d857327d36ab9b9c
Duda Nunes <eduardapmn@gmail.com>
2023-04-14 00:06:32 -0300

    Profile PixelArt Added

--- Js/lightmode.js
+++ Js/lightmode.js
@@ -3,10 +3,13 @@ var btn_light_Dark = document.querySelector('.btn-light-dark');
 btn_light_Dark.addEventListener('click',lightMode);
 
 function lightMode(){
-    var light = document.querySelector('.light') 
-    var dark = document.querySelector('.dark')
+    var light = document.querySelectorAll('.light') 
+    var dark = document.querySelectorAll('.dark')
 
     document.body.classList.toggle('lightmode')
-    light.classList.toggle('hide')
-    dark.classList.toggle('hide')
+
+    for (i=0;i<light.length;i++){
+        light[i].classList.toggle('hide')
+        dark[i].classList.toggle('hide')     
+    }
 }
