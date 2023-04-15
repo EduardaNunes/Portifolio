c6d1af89041bf88d86b64f73a0bf8e360c850a71
Duda Nunes <eduardapmn@gmail.com>
2023-04-15 15:02:02 -0300

    Criação aleatória das nuvens
    -Ainda não resetam a animação da forma correta
    -Ainda não se mexem pra cima e para baixo

--- Js/clouds.js
+++ Js/clouds.js
@@ -1,37 +1,15 @@
-var cloudsSection = document.querySelector('.clouds')
+const cloudsSection = document.querySelector('.clouds')
 
-for (var i = 0; i < 25; i++){
-    cloudsSection.innerHTML+='<div class="cloud cloud'+[i]+'"></div>';
+for (var i = 0; i < 12; i++){
+    let cloudCreate = document.createElement('img');
+    cloudCreate.classList.add('cloud');
+    cloudsSection.appendChild(cloudCreate)
+
     var cloud = document.querySelectorAll('.cloud');
+    var randomAnimation = Math.floor(Math.random()*10)+ 25;
+    var randomPosition = Math.floor(Math.random()*101);
 
-        while (i<5){
-            cloud[i].style.top += (-5) + 'vh';
-            cloud[i].style.left += (i * 20+2) + 'vw';
-            break;
-        }
-
-        while (i>=5 && i<10){
-            cloud[i].style.top += (20) + 'vh';
-            cloud[i].style.left += ((i-5) * 20+15) + 'vw';
-            break
-        }
-
-        while (i>=10 && i<15){
-            cloud[i].style.top += (45) + 'vh';
-            cloud[i].style.left += ((i-10) * 20+2) + 'vw';
-            break
-        }
-
-        while (i>=15 && i<20){
-            cloud[i].style.top += (70) + 'vh';
-            cloud[i].style.left += ((i-15) * 20+15) + 'vw';
-            break
-        }
-
-        while (i>=20 && i<25){
-            cloud[i].style.top += (95) + 'vh';
-            cloud[i].style.left += ((i-20) * 20+2) + 'vw';
-            break
-        }
-
+    cloud[i].style.top = (8.3*i) + 'vh';
+    cloud[i].style.left = randomPosition + 'vw';
+    cloud[i].style.animation = 'across ' + randomAnimation + 's linear infinite';
 }
